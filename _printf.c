45d11ba924e04a4a93b8d5033b4128d5d544f83b
ialag <ialaghita94@gmail.com>
2023-05-27 14:33:42 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,6 +1,38 @@
 #include "main.h"
 
 /**
+ * flags - function that checks flags
+ * @type: specifier
+ * @length: length string
+ *
+ * Return: 1 if puts smtg, 0 if not
+ */
+int flags(const char *type, int *length)
+{
+	if (*type == ' ')
+	{
+		*length += _putchar(' ');
+		return (1);
+	}
+	else if (*type == '+')
+	{
+		*length += _putchar('+');
+		return (1);
+	}
+	else if (*type == '#')
+	{
+		if (*(type + 1) == 'o')
+			*length = _putchar('0');
+		if (*(type + 1) == 'x' || *(type + 1) == 'X')
+			*length = _printf("0x");
+		return (1);
+	}
+	return (0);
+}
+
+
+
+/**
  * _printf - function that print the formatted string
  * @format: formatted string
  *
