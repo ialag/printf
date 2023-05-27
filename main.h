b1f76a325d0ae749e168129c51be132d6c9cd651
ialag <ialaghita94@gmail.com>
2023-05-27 14:36:15 -0700

    main.h

--- main.h
+++ main.h
@@ -14,7 +14,7 @@ int _puts(char *string);
 void custom_specifiers(va_list list, char type, int *length);
 
 void print_number(long n, int *length);
-
+int flags(const char *type, int *length);
 void *_realloc(void *p, unsigned int old_size, unsigned int new_size);
 
 void _unsigned_int(unsigned long n, int *length);
