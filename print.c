840d104eb401ac662aaaf2a28e46ccd98674a357
ialag <ialaghita94@gmail.com>
2023-05-27 16:15:31 -0700

    print.c

--- print.c
+++ print.c
@@ -18,8 +18,8 @@ void custom_specifiers(va_list list, char type, int *length)
 		(*length) += _putchar(va_arg(list, int));
 	else if (type == 's')
 		(*length) += _puts(va_arg(list, char*));
-	else if (type == 'i' || type == 'd')
-		print_number(va_arg(list, int), length);
+	/*else if (type == 'i' || type == 'd')
+		print_number(va_arg(list, int), length);*/
 	/*else if (type == 'o')*/
 		/*(*length) = (*length) + print_octal(va_arg(list, int));*/
 }
