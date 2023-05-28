fef05227207c040b4565556868fc2063f1baf14a
ialag <ialaghita94@gmail.com>
2023-05-28 13:17:59 -0700

    printf project

---
+++ print_number11.c
@@ -0,0 +1,127 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if character is digit
+ * @c: the characters to check
+ * Return: 1 if digit, 0 otherwise
+*/
+
+int _isdigit(int c)
+{
+return (c >= '0' && c <= '9');
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ * Return: integer length of string
+*/
+
+int _strlen(char *s)
+{
+int i = 0;
+while (*s++)
+i++;
+return (i);
+}
+
+/**
+ * print_number - prints a number with options
+ * @string: the base number as a string
+ * @params: the parameter struct
+ * Return: chars printed
+*/
+
+int print_number(char *string, params_t *params)
+{
+unsigned int i = _strlen(string);
+int neg = (!params->unsign && *string == '-');
+if (!params->precision && *string == '0' && !string[1])
+string = "";
+if (neg)
+{
+string++;
+i--;
+}
+if (params->precision != UINT_MAX)
+while (i++ < params->precision)
+*--string = '0';
+if (neg)
+*--string = '-';
+if (!params->minus_flag)
+return (print_number_right_shift(string, params));
+else
+return (print_number_left_shift(string, params));
+}
+
+/**
+ * print_number_right_shift - prints a nbr with options
+ * @string: the base nbr as a str
+ * @params: the parameter struct
+ * Return: chars printed
+*/
+
+int print_number_right_shift(char *string, params_t *params)
+{
+unsigned int n = 0, neg, neg2, i = _strlen(str);
+char pad_char = ' ';
+if (params->zero_flag && !params->minus_flag)
+pad_char = '0';
+neg = neg2 = (!params->unsign && *string == '-');
+if (neg && i < params->width && pad_char == '0' && !params->minus_flag)
+string++;
+else
+neg = 0;
+if ((params->plus_flag && !neg2) ||
+(!params->plus_flag && params->space_flag && !neg2))
+i++;
+if (neg && pad_char == '0')
+n += _putchar('-');
+if (params->plus_flag && !neg2 && pad_char == '0' && !params->unsign)
+n += _putchar('+');
+else if (!params->plus_flag &&
+	params->space_flag &&
+	!neg2 && !params->unsign && params->zero_flag)
+n += _putchar(' ');
+while (i++ < params->width)
+n += _putchar(pad_char);
+if (neg && pad_char == ' ')
+n += _putchar('-');
+if (params->plus_flag && !neg2 && pad_char == ' ' && !params->unsign)
+n += _putchar('+');
+else if (!params->plus_flag &&
+	 params->space_flag &&
+	 !neg2 &&
+	 !params->unsign &&
+	 !params->zero_flag)
+n += _putchar(' ');
+n += _puts(string);
+return (n);
+}
+
+/**
+ * print_number_left_shift - prints a nbr with options
+ * @string: base nbr as a sting
+ * @params: the parameters struct
+ * Return: chars printed
+*/
+int print_number_left_shift(char *string, params_t *params)
+{
+unsigned int n = 0, neg, neg2, i = _strlen(string);
+char pad_char = ' ';
+if (params->zero_flag && !params->minus_flag)
+pad_char = '0';
+neg = neg2 = (!params->unsign && *string == '-');
+if (neg && i < params->width && pad_char == '0' && !params->minus_flag)
+string++;
+else
+neg = 0;
+if (params->plus_flag && !neg2 && !params->unsign)
+n += _putchar('+'), i++;
+else if (params->space_flag && !neg2 && !params->unsign)
+n += _putchar(' '), i++;
+n += _puts(string);
+while (i++ < params->width)
+n += _putchar(pad_char);
+return (n);
+}
---
+++ simple_printers11.c
@@ -0,0 +1,76 @@
+#include "main.h"
+/**
+ * print_from_to - prints a range of char addr
+ * @start: start addr
+ * @stop: stop addr
+ * @except: expt addr
+ * Return: nbr bytes printed
+*/
+int print_from_to(char *start, char *stop, char *except)
+{int sum = 0;
+while (start <= stop)
+{
+if (start != except)
+{sum += _putchar(*start); }
+start++; }
+return (sum); }
+/**
+ * print_rev - prints string in reverse
+ * @ap: string
+ * @params: the parameters struct
+ * Return: number of bytes printed
+*/
+int print_rev(va_list ap, params_t *params)
+{int len, sum = 0;
+char *string = va_arg(ap, char *);
+(void)params;
+if (string)
+{
+for (len = 0; string[len]; len++)
+;
+string--;
+for (; len > 0; len--, string--)
+{
+sum += _putchar(*string); }
+}
+return (sum); }
+/**
+ * print_rot13 - prints string in rot13
+ * @ap: string
+ * @params: the parameters struct
+ * Return: number of bytes printed
+*/
+int print_rot13(va_list ap, params_t *params)
+{
+char *a = va_arg(ap, char *);
+(void)params;
+if (a)
+{
+return (print_rot13_string(a)); }
+return (0); }
+/**
+ * print_rot13_string - prints a string in rot13
+ * @str: the string to print
+ * Return: nbr of bytes printed
+*/
+int print_rot13_string(char *str)
+{
+int count = 0;
+char arr[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+while (*str)
+{
+if ((*str >= 'A' && *str <= 'Z') || (*str >= 'a' && *str <= 'z'))
+{
+int index = *str - 'A';
+if (*str >= 'a' && *str <= 'z')
+index = *str - 'a' + 26;
+count += _putchar(arr[index]);
+}
+else
+{
+count += _putchar(*str);
+}
+str++;
+}
+return (count); }
+
