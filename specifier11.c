7564df0976c7ec8b375191c70e6d3c5428b0fba5
ialag <ialaghita94@gmail.com>
2023-05-28 14:10:32 -0700

    rt

--- specifier11.c
+++ specifier11.c
@@ -87,21 +87,6 @@ return (i);
 */
 int get_modifier(char *s, params_t *params)
 {
-int i = 0;
-switch (*s)
-{
-case 'h':
-i = params->h_modifier = 1;
-break;
-case 'l':
-i = params->l_modifier = 1;
-break;
-}
-return (i);
-}
-
-int get_modifier(char *s, params_t *params)
-{
 	int i = 0;
 
 	switch (*s)
